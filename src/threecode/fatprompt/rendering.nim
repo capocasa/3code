5d962a1f7e1e412484d88c33cf21343892b06ce4
3code <3code@capocasa.dev>
2026-07-02 12:06:33 +0200

    ctrlc cursor position

--- src/threecode/fatprompt/rendering.nim
+++ src/threecode/fatprompt/rendering.nim
@@ -495,10 +495,10 @@ proc formatUserPromptItem*(line: string): string =
   result.addUserEcho(line, trailingNewline = false)
 
 proc promptOnlyBytes*(): string =
-  "\r\x1b[2K" & EditorPromptBytes & "\r"
+  "\r\x1b[2K" & EditorPromptBytes
 
 proc promptOnlyResetBytes*(): string =
-  "\x1b[2K" & EditorPromptBytes & "\r"
+  "\x1b[2K" & EditorPromptBytes
 
 proc clearPromptAfterPendingReceiptBytes*(): string =
   "\r\x1b[1A\x1b[J"
