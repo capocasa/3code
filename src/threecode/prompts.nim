205dac19679a457ddc0d83be65782e2a54c67fc9
3code <3code@capocasa.dev>
2026-07-07 15:01:09 +0200

    add version command

--- src/threecode/prompts.nim
+++ src/threecode/prompts.nim
@@ -1273,6 +1273,7 @@ commands:
   :log              list all tool calls this session
   :sessions         list recent sessions saved in this directory (max 20)
   :summarize        collapse old turns into a synthetic recap (meta model call)
+  :version          show the running 3code version
   :q :quit          exit (also Ctrl-D)
 
 input:
